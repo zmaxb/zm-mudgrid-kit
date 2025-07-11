f2b6353381d91b57372169934ea52a578f02a029
zmaxb <zmaxbraun@gmail.com>
2025-07-11 18:05:25 +0400

    feat: update default DataGrid page size to 50
    - Changed the `DefaultPageSize` in `DataGridOptions` from 10 to 50 to enhance usability for larger datasets.

--- Zm.MudGridKit/DataGrid/DataGridOptions.cs
+++ Zm.MudGridKit/DataGrid/DataGridOptions.cs
@@ -11,6 +11,6 @@ public class DataGridOptions
     public bool ShowColumnOptions { get; set; } = false;
     public bool ShowFilterIcons { get; set; } = false;
     public bool ShowPager { get; set; } = true;
-    public int DefaultPageSize { get; set; } = 10;
+    public int DefaultPageSize { get; set; } = 50;
     public int[] PageSizeOptions { get; set; } = [10, 25, 50, 100];
 }
